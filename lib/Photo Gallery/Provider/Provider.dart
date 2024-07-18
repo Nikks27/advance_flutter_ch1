6c3f4b8410890616c019aa250a891ffe009c9491
Nareshbhai Rabari <nareshrabari205@gmail.com>
2024-07-18 12:23:14 +0530

    changetheme stepper provider change theme QoutesaddData Time intro Screen

---
+++ lib/Photo Gallery/Provider/Provider.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+
+class GalleryProvider extends ChangeNotifier {
+  final localAuth = LocalAuthentication();
+  bool didAuthenticate = false;
+
+  Future<void> authication() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await localAuth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      'device not supported';
+    }
+    try {
+      didAuthenticate = await localAuth.authenticate(
+        localizedReason: 'Please authenticate to access secure data',
+      );
+      didAuthenticate = didAuthenticate;
+      notifyListeners();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+}
